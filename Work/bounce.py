a47cdad84a3d5c06dd690ea3896111b6a4cda22c
su1ek <barteksulkowski7@gmail.com>
2026-04-29 22:17:44 +0200

    first course progress

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+num_ofBounces = 0
+height = 100 # meters
+for i in range(10):
+    num_ofBounces += 1
+    height = height * 3 / 5
+    print(num_ofBounces, round(height, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,34 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+base_payment = 2684.11
+total_paid = 0.0
+months = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    months += 1
+
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        payment = base_payment + extra_payment
+    else:
+        payment = base_payment
+
+    principal = principal * (1+rate/12) - payment
+
+    if principal < 0:
+        total_paid = total_paid - abs(principal)
+        principal = 0.0
+
+    total_paid += payment
+    left_to_pay = principal
+
+    print(round(total_paid, 2), round(left_to_pay, 2))
+
+print('Total paid', round(total_paid,2))
+print('Months', months)
