086588d22ae69360b937578d0f77ad2f8ecec89a
su1ek <barteksulkowski7@gmail.com>
2026-05-11 16:06:54 +0200

    Added csv library

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,18 +2,20 @@
 #
 # Exercise 1.27
 
+import csv
+
 def portfolio_cost(filename):
     with open(filename, 'rt') as f:
         total_cost = 0.0
-        headers = next(f)
-        for line in f:
-            row = line.split(',')
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
             try:
                 total_cost += int(row[1]) * float(row[2])
             except ValueError:
                 continue
     return total_cost
 
-cost = portfolio_cost('Data/missing.csv')
+cost = portfolio_cost('Data/portfolio.csv')
 print('Total cost:', cost)
 
